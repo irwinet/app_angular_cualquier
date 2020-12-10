7c02c149a41b6fce492df5543e4e7550115e917f
irwinet <irwinestradatorres@gmail.com>
2020-12-10 11:43:17 -0500

    Add Navigation Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { Pagina1Component } from './pages/pagina1/pagina1.component';
 import { Pagina2Component } from './pages/pagina2/pagina2.component';
 import { Pagina3Component } from './pages/pagina3/pagina3.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Pagina1Component,
     Pagina2Component,
-    Pagina3Component
+    Pagina3Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
